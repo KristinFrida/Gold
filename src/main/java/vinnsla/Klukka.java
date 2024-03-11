b9b3f54abdb62b8eb42d70de9c6477dc0b07b542
KristinFrida <kfs14@hi.is>
2024-03-11 17:25:57 +0000

    Klukka breytist úr 60 í 59

--- src/main/java/vinnsla/Klukka.java
+++ src/main/java/vinnsla/Klukka.java
@@ -1,10 +1,14 @@
 package vinnsla;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.util.Duration;
 
 public class Klukka {
     private SimpleIntegerProperty timi;
+    private Timeline timeline;
 
     // Smiður fyrir Klukka klasann
     public Klukka(int byrjaTima) {
@@ -13,8 +17,24 @@ public class Klukka {
 
     // Aðferð sem telur tímann niður
     public void tic() {
-        if (timi.get() > 0) {
-            timi.set(timi.get() - 1);
+        if (timeline != null) {
+            timeline.stop();
+        }
+
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            if (timi.get() > 0) {
+                timi.set(timi.get() - 1);
+            }
+        }));
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    // Aðferð sem stöðvar klukkuna
+    public void stop() {
+        if (timeline != null) {
+            timeline.stop();
         }
     }
 
